e896cb8dc23ca08c4fb8456751c715844066230d
Brayan <gomezmancobrayanalexander@gmail.com>
2020-12-03 22:20:41 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+//=============================================
+// EFFECT HEADER 
+//=============================================
+
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+})
+
+function toggle(){
+    var header = document.querySelector('header');
+    header.classList.toggle('active');   
+}
